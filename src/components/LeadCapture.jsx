52a1289198243000a99de7ffb5e4ba84b47937dd
Pedro Alves <pedromarcelodesaalves@gmail.com>
2016-07-24 16:20:50 -0300

    Name field component

--- src/components/LeadCapture.jsx
+++ src/components/LeadCapture.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
+var NameField = require('./NameField.jsx');
+var EmailField = require('./EmailField.jsx');
 
 var LeadCapture = React.createClass({
+    onSubmit: function() {
+        
+    },
     render: function() {
         return (
             <div className="col-ms-3">
                 <div className="panel panel-default">
                     <div className="panel-body">
-
+                        <NameField type="First" />
+                        <EmailField />
+                        <button className="btn btn-primary" onClick={this.onSubmit}>
+                            Submit
+                        </button>
                     </div>
                 </div>
             </div>
---
+++ src/components/NameField.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var NameField = React.createClass({
+    getInitialState: function() {
+        return {value: ""};
+    },
+    onChange: function(e) {
+        this.setState({value: e.target.value});
+    },
+    render: function() {
+        return (
+            <input className="form-control"
+                placeholder={this.props.type + " Name"}
+                onChange={this.onChange}
+                value={this.state.value} />
+        );
+    }
+});
